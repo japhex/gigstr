ac07ef8a574fefab27dac877621bc039e8ba31d7
japhex <j@japhex.net>
2023-02-08 10:58:26 +0000

    api keys in secrets

--- api/apis/ticketmaster.ts
+++ api/apis/ticketmaster.ts
@@ -1,11 +1,16 @@
 import axios from 'axios'
 import { formatISO } from 'date-fns'
 
+import { getSecrets } from '../utils/aws'
+
 export const ticketmasterApi = {
-  eventSearch: async (artist, page = 0) =>
-    axios.get(
+  eventSearch: async (artist, page = 0) => {
+    const secrets = await getSecrets()
+
+    return axios.get(
       `https://app.ticketmaster.com/discovery/v2/events?apikey=${
-        process.env.TICKET_MASTER_API_KEY
+        secrets.TICKET_MASTER_API_KEY
       }&locale=*&keyword=${artist}&segmentName=music&startDateTime=${formatISO(new Date())}&sort=date,asc&page=${page}`
-    ),
+    )
+  },
 }
