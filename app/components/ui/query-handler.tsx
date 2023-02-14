fcfd82c4aa9d68adc87e56842cfd965f928d9084
japhex <j@japhex.net>
2023-02-14 10:48:32 +0000

    skeleton loader tweak

--- app/components/ui/query-handler.tsx
+++ app/components/ui/query-handler.tsx
@@ -1,4 +1,3 @@
-import { Box } from '@chakra-ui/react'
 import SkeletonLoader from '@components/ui/skeleton-loader'
 import SkeletonLoaderThin from '@components/ui/skeleton-loader-thin'
 import UseAnimations from 'react-useanimations'
@@ -14,13 +13,13 @@ interface Props {
 
 const QueryHandler = ({ loading, error, strokeColor, loop = 8, loaderStyle = 'default' }: Props) => {
   return (
-    <Box>
+    <>
       {loading &&
         (loaderStyle === 'default'
           ? [...Array(loop)].map(() => <SkeletonLoader />)
           : [...Array(3)].map(() => <SkeletonLoaderThin />))}
       {error && <UseAnimations strokeColor={strokeColor} animation={alertCircle} />}
-    </Box>
+    </>
   )
 }
 
