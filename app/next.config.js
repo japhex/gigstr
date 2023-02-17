00665d37eadebc6be74a5d26160f8e97583ea02d
japhex <j@japhex.net>
2023-02-17 16:32:52 +0000

    add cache headers for images

--- app/next.config.js
+++ app/next.config.js
@@ -8,4 +8,18 @@ module.exports = {
   images: {
     domains: ['gigstr.s3.amazonaws.com'],
   },
+  async headers() {
+    return [
+      {
+        source: '/:all*(svg|jpg|png)',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=9999999999, must-revalidate',
+          },
+        ],
+      },
+    ]
+  },
 }
